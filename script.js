e12db419b69aaa98828f1c26a8162c4e16c575d2
Dario Alarcon <darioalarcon832@gmail.com>
2024-03-02 11:09:56 -0500

    animations

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const observedIcons = document.querySelectorAll('.icon-div');
+const observedslider = document.querySelectorAll('.tranding');
+const observedbath = document.querySelectorAll('.div-baño');
+    const mainImg = document.getElementById('main-div-left');
+    const info = document.getElementById('info-cuartos');
+    const infor = document.getElementById('info-cocina');
+    const loadPhrase = (entries, observer)=>{
+      entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('visible');
+              console.log(entry.target.classList)
+          }
+      });
+    }
+    const observer = new IntersectionObserver(loadPhrase,{
+      root: null,
+      rootMargin: "0px 0px -100px 0px",
+      threshold: 0.5
+    },
+    )
+    const observar = new IntersectionObserver(loadPhrase,{
+      root: null,
+      rootMargin: "0px 0px -10px 0px",
+      threshold: 0.2
+    },
+    )
+    observedIcons.forEach(element => {
+        observer.observe(element);
+    });
+    observedslider.forEach(element => {
+        observer.observe(element);
+    });
+    observedbath.forEach(element => {
+        observar.observe(element);
+    });
+    observer.observe(mainImg);
+    observer.observe(info);
+    observer.observe(infor);
+
+
+
 var TrandingSlider = new Swiper('.tranding-slider', {
     grabCursor: true,
     autoplay:{
@@ -16,3 +57,4 @@ var TrandingSlider = new Swiper('.tranding-slider', {
       prevEl: '.swiper-button-prev',
     }
   });
+
